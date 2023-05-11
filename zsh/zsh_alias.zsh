8b96867b9af82f058bcbcb2cb668928c10c13cf8
jliocsar <juliocesarmfo@gmail.com>
2023-05-11 00:31:22 -0300

    chore: remove unused stuff

--- zsh/zsh_alias.zsh
+++ zsh/zsh_alias.zsh
@@ -23,14 +23,6 @@ alias nr='npm run'
 alias ni='npm install'
 alias nid='npm install --save-dev'
 
-## toggl
-# alias today='toggl l today'
-
-# release
-alias check-version='conventional-recommended-bump -p angular -v'
-alias create-changelog='conventional-changelog -p angular -i CHANGELOG.md -s && git add CHANGELOG.md && git commit -m 'docs: Update changelog file''
-alias create-release='conventional-github-releaser -p angular'
-
 # bash
 if command -v tree &> /dev/null; then
   alias ls='tree -L 1'
