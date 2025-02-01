699ff24353d92d9205fbafda2fc1d3dadb2b1d98
jliocsar <juliocesarmfo@gmail.com>
2025-02-01 20:29:35 -0300

    sync

--- dotstorage/.wezterm.lua
+++ dotstorage/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Framer'
+config.color_scheme = 'Edge Dark (base16)'
 config.font = wezterm.font('Victor Mono', {
     weight = 'DemiBold'
 })
