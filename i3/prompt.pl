d5dd316c0e2ed417c03d09246788f21d557cb1a6
jliocsar <juliocesarmfo@gmail.com>
2025-02-09 22:11:26 -0300

    sync

--- i3/prompt.pl
+++ i3/prompt.pl
@@ -14,10 +14,11 @@ unless (defined $opt_type) {
     exit 1;
 }
 
+my $MEINE_DMENU_THEME = "Pop-Dark";
+
 sub meine_i3_prompt {
     my $placeholder = shift;
-    my $msg = shift;
-    chomp(my $result = `i3-input -F '$msg' -P '$placeholder' `);
+    chomp(my $result = `rofi -dmenu -theme $MEINE_DMENU_THEME  -theme-str 'listview { lines: 0; border: 1px; } entry { placeholder: ""; }' -p '$placeholder'`);
     return $result;
 }
 
@@ -34,39 +35,11 @@ sub notify {
 if ($opt_type eq "rename-ws") {
     chomp(my $current_i3_workspaces = `i3-msg -t get_workspaces`);
     my @current_i3_workspaces = @{decode_json($current_i3_workspaces)};
-    my @workspace_names = map { $_->{name} } @current_i3_workspaces;
-    my $names = join("|", sort @workspace_names);
+    my @workspace_names = map { "$_->{name}" } @current_i3_workspaces;
+    my $names = join(", ", sort @workspace_names);
 
-    my $workspace_name_result = meine_i3_prompt "Rename workspace <$names>: ", "echo %s";
-    $workspace_name_result = `echo "$workspace_name_result" | tail -1`;
-    $workspace_name_result =~ s/command = echo //;
-    chomp $workspace_name_result;
-
-    print($workspace_name_result);
-
-    my $workplace_to_rename;
-    for my $workspace (@current_i3_workspaces) {
-        my $workspace_name = $workspace->{name};
-        if ($workspace_name =~ "^$workspace_name_result.*") {
-            $workplace_to_rename = $workspace_name;
-            last;
-        }
-    }
-
-    if ($workplace_to_rename) {
-        chomp $workplace_to_rename;
-    } else {
-        my $visible_workspace;
-        for my $workspace (@current_i3_workspaces) {
-            if ($workspace->{visible}) {
-                $visible_workspace = $workspace->{name};
-                last;
-            }
-        }
-    }
-
-    # Most likely canceled the prompt
-    unless ($workplace_to_rename) {
+    my $workspace_name_result = meine_i3_prompt "Rename workspace ($names or \@me)";
+    if ($workspace_name_result eq "") {
         notify "Workspace rename", "Canceled", {
             urgency => "low",
             time => 3000,
@@ -74,19 +47,57 @@ if ($opt_type eq "rename-ws") {
         exit 0;
     }
 
-    chomp(my $workspace_new_name_result = meine_i3_prompt "Enter new name for workspace <$workplace_to_rename>: ", "echo %s");
-    chomp($workspace_new_name_result = `echo "$workspace_new_name_result" | tail -1`);
-    $workspace_new_name_result =~ s/command = echo //;
-    chomp $workspace_new_name_result;
+    my $workspace_to_rename = $workspace_name_result;
+    if ($workspace_to_rename eq "\@me") {
+        my $visible_workspace;
+        for my $workspace (@current_i3_workspaces) {
+            if ($workspace->{visible}) {
+                $visible_workspace = $workspace->{name};
+                last;
+            }
+        }
+        $workspace_to_rename = $visible_workspace;
+    } else {
+        for my $workspace (@current_i3_workspaces) {
+            my $workspace_name = $workspace->{name};
+            if ($workspace_name =~ "^$workspace_name_result.*") {
+                $workspace_to_rename = $workspace_name;
+                last;
+            }
+        }
+    }
 
-    my $workspace_id = $workplace_to_rename =~ s/:.*//r;
+    if ($workspace_to_rename) {
+        chomp $workspace_to_rename;
+    } else {
+        notify "Workspace rename", "Workspace not found", {
+            urgency => "low",
+            time => 3000,
+        };
+        exit 0;
+    }
+
+    chomp(my $workspace_new_name_result = meine_i3_prompt "Enter new name for workspace ($workspace_to_rename or :reset)");
+    if ($workspace_new_name_result eq "") {
+        notify "Workspace rename", "Canceled", {
+            urgency => "low",
+            time => 3000,
+        };
+        exit 0;
+    }
+
+    if ($workspace_new_name_result eq ":reset") {
+        $workspace_new_name_result = "";
+    }
+
+    my $workspace_id = $workspace_to_rename =~ s/:.*//r;
     my $new_workspace = $workspace_id;
 
     unless ($workspace_new_name_result eq "") {
         $new_workspace = "$new_workspace:$workspace_new_name_result";
     }
 
-    system("i3-msg 'rename workspace \"$workplace_to_rename\" to \"$new_workspace\"'");
+    system("i3-msg 'rename workspace \"$workspace_to_rename\" to \"$new_workspace\"'");
     exit 0;
 }
 
