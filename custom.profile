a5b11eeb43d557e373663e80ba9900cb6a2da62c
jliocsar <juliocesarmfo@gmail.com>
2025-02-24 13:40:29 -0300

    sync

--- custom.profile
+++
@@ -1,3 +0,0 @@
-## meine custom.profile section
-export __MEINE_BG_COLOR="#222222"
-## /end meine custom.profile
--- dotstorage/.wezterm.lua
+++ dotstorage/.wezterm.lua
@@ -1,12 +1,15 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
-local plugin = wezterm.plugin
+-- local plugin = wezterm.plugin
 
-config.color_scheme = 'Circus (base16)'
+-- ## Font Configuration
 config.font_size = 11.0
 config.font = wezterm.font('Victor Mono', {
   weight = 'DemiBold',
 })
+
+-- ## Colors
+config.color_scheme = 'Circus (base16)'
 config.window_background_gradient = {
   -- Can be "Vertical" or "Horizontal".  Specifies the direction
   -- in which the color gradient varies.  The default is "Horizontal",
@@ -75,9 +78,85 @@ config.window_background_gradient = {
   -- segment_size = 11,
   -- segment_smoothness = 0.0,
 }
-config.enable_tab_bar = false
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = 'rgba(0, 0, 0, 0.1)',
 
--- Plugins
-plugin.require('https://github.com/yriveiro/wezterm-status').apply_to_config(config, {})
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = 'rgba(0, 0, 0, 0.1)',
+      -- The color of the text for the tab
+      fg_color = '#ccc',
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
+
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
+
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
+
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = false,
+    },
+
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      bg_color = 'rgba(0, 0, 0, 0.1)',
+      fg_color = '#666',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over inactive tabs
+    inactive_tab_hover = {
+      bg_color = 'rgba(255, 255, 255, 0.025)',
+      fg_color = '#666',
+      italic = false,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab_hover`.
+    },
+
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = 'rgba(255, 255, 255, 0.025)',
+      fg_color = '#666',
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over the new tab button
+    new_tab_hover = {
+      bg_color = '#999',
+      fg_color = 'rgba(0, 0, 0, 0.1)',
+      italic = false,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab_hover`.
+    },
+  },
+}
+
+-- ## Tabs Configuration
+config.enable_tab_bar = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+
+-- ## Plugins
 
 return config
